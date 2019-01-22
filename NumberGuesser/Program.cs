125cc60fc80d56f9d129c9850c700a02a93cb13b
Piyush Mehta <me@piyushmehta.com>
2019-01-22 21:41:54 +0530

    Checked

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -7,7 +7,10 @@ namespace NumberGuesser
         static void Main(string[] args)
         {
             string name = "Piyush Mehta";
-            Console.WriteLine("Hello " + name);
+            int age = 35;
+
+            Console.WriteLine(name + " is" + age);
+            Console.WriteLine("{0} is {1}", name, age);
         }
     }
 }
