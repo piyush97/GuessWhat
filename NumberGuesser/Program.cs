877fa2b2b006e4e240cff80da39c4bd43f894ede
Piyush Mehta <me@piyushmehta.com>
2019-01-22 21:46:21 +0530

    color

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -7,11 +7,18 @@ namespace NumberGuesser
         static void Main(string[] args)
         {
             // Set app vars
-            string appName = "Gues It";
+            string appName = "Guess It";
             string appVersion = "1.0.0";
             string appAuthor = "Piyush Mehta";
 
+            // Change in TextColor
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // Write out app info
             Console.WriteLine("{0}: Version {1} by {2}", appName,appVersion,appAuthor);
+
+            // Reset Text color
+            Console.ResetColor();
         }
     }
 }
